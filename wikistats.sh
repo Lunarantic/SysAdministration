c8d2d69d175d185759bc3a019af4e5df92062c4d
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2018-04-09 16:46:59 -0400

    Update wikistats.h
    corrected assignment of flags and desc variable when flags found empty

--- wikistats.sh
+++ wikistats.sh
@@ -39,91 +39,89 @@ desc=""
 
 while [ "$1" != "" ] ; do
 	case $1 in
-	"-h")	help ;;
-	"-b")	flags=$flags" b" ;;
-	"-f")	flags=$flags" f" ;;
-    "-l")	flags=$flags" l" ;;
-    "-r")	flags=$flags" r" ;;
-    "-u")	flags=$flags" u" ;;
-    "-d")	shift
-		if [ "$1" = "all" ]; then
-			lang=""
-		else
-			lang=$1
-		fi ;;
-	"-"*)	if [ ${#1} -le 2 ] ; then
-			help
-	    	fi
-		    case $1 in
-		        *h*) help
-		    esac
-		    case $1 in
-		        *b*) flags=$flags" b"
-		    esac
-            case $1 in 
-                *f*) flags=$flags" f"
-            esac
-            case $1 in 
-                *l*) flags=$flags" l"
-            esac
-            case $1 in 
-                *r*) flags=$flags" r"
-            esac
-            case $1 in 
-                *u*) flags=$flags" u"
-            esac
-		case $1 in
-		    *d*)	shift
-			    if [ "$1" = "all" ]; then
-				    lang=""
-			    else
-				    lang=$1
-			    fi ;;
-		    *b*)	;;
-		    *f*)	;;
-    		*l*)	;;
-	    	*r*)	;;
-		    *u*)	;;
-		    *)	help
+		"-h")	help ;;
+		"-b")	flags=$flags" b" ;;
+		"-f")	flags=$flags" f" ;;
+    	"-l")	flags=$flags" l" ;;
+    	"-r")	flags=$flags" r" ;;
+    	"-u")	flags=$flags" u" ;;
+    	"-d")	shift
+				if [ "$1" = "all" ]; then
+					lang=""
+				else
+					lang=$1
+			fi ;;
+		"-"*)	if [ ${#1} -le 2 ] ; then
+					help
+	  		  	fi
+			    case $1 in
+			        *h*) help
+			    esac
+			    case $1 in
+			        *b*) flags=$flags" b"
+			    esac
+    	        case $1 in 
+    	            *f*) flags=$flags" f"
+    	        esac
+    	        case $1 in 
+    	            *l*) flags=$flags" l"
+    	        esac
+    	        case $1 in 
+      	        	*r*) flags=$flags" r"
+     	        esac
+     	        case $1 in 
+      	        	*u*) flags=$flags" u"
+	   	        esac
+				case $1 in
+			    	*d*)	shift
+			    			if [ "$1" = "all" ]; then
+				    			lang=""
+						    else
+							    lang=$1
+						    fi ;;
+		    		*b*)	;;
+				    *f*)	;;
+		    		*l*)	;;
+	    			*r*)	;;
+			    	*u*)	;;
+			    	*)	help
+				esac
+				desc="1" ;;
+		*)		filename=$1
+
+				if [ "$flags" = "" ] ; then
+					flags="u f b r l"
+					desc="1"
+				fi
+
+				for f in $flags
+				do
+					case $f in
+						*b*)	if [ "$desc" != "" ] ; then
+								    echo -n "Total bytes transferred: "
+				    			fi
+						    	bytes $filename $lang ;;
+						*f*)	if [ "$desc" != "" ] ; then
+		    						echo -n "Most frequent object: "
+			    				fi
+				    			frequent $filename $lang ;;
+						*l*)	if [ "$desc" != "" ] ; then
+		    						echo -n "Largest object: "
+			    				fi
+				    			largest $filename $lang ;;
+						*r*)	if [ "$desc" != "" ] ; then
+		    						echo -n "Requests per second: "
+		    					fi
+		    					reqpersec $filename $lang ;;
+						*u*)	if [ "$desc" != "" ] ; then
+					    			echo -n "Unique objects: "
+			    				fi
+			    				unique $filename $lang ;;
+						*)	help
+					esac
+				done
+				exit
 		esac
-		desc="1" ;;
-	*)
-		filename=$1
-
-		if [ "$flags" = "" ] ; then
-			$flags="u f b r l"
-			$desc="1"
-		fi
-
-		for f in $flags
-		do
-			case $f in
-				*b*)	if [ "$desc" != "" ] ; then
-						    echo -n "Total bytes transferred: "
-				    	fi
-				    	bytes $filename $lang ;;
-				*f*)	if [ "$desc" != "" ] ; then
-		    				echo -n "Most frequent object: "
-			    		fi
-		    			frequent $filename $lang ;;
-				*l*)	if [ "$desc" != "" ] ; then
-		    				echo -n "Largest object: "
-		    			fi
-		    			largest $filename $lang ;;
-				*r*)	if [ "$desc" != "" ] ; then
-		    				echo -n "Requests per second: "
-		    			fi
-		    			reqpersec $filename $lang ;;
-				*u*)	if [ "$desc" != "" ] ; then
-			    			echo -n "Unique objects: "
-			    		fi
-			    		unique $filename $lang ;;
-				*)	help
-			esac
-		done
-		exit
-	esac
-	shift
+		shift
 done
-
 help
